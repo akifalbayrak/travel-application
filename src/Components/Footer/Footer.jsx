1d3a6d1522d6abe5eeb55830f6941fd8c78cfba3
Akif Albayrak <akifalbayrak6161@gmail.com>
2024-01-25 18:19:23 +0300

    adding main and footer & adding little styling

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,135 @@
-import React from 'react'
-import "./footer.css"
+import React from "react";
+import "./footer.css";
+import video from "../Assets/video.mp4";
+import { FiSend } from "react-icons/fi";
+import { MdOutlineTravelExplore } from "react-icons/md";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { AiFillYoutube } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaTripadvisor } from "react-icons/fa";
+import { FiChevronRight } from "react-icons/fi";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+        <section className="footer">
+            <div className="videoDiv">
+                <video src={video} loop autoPlay muted type="video/mp4"></video>
+            </div>
+            <div className="secContent container">
+                <div className="contactDiv flex">
+                    <div className="text">
+                        <small>KEEP IN TOUCH</small>
+                        <h2>Travel with us</h2>
+                    </div>
+                    <div className="inputDiv flex">
+                        <input type="text" placeholder="Enter Email Adress" />
+                        <button className="btn flex" type="submit">
+                            Send <FiSend className="icon" />
+                        </button>
+                    </div>
+                </div>
+                <div className="footerCard flex">
+                    <div className="footerIntro flex">
+                        <div className="logoDiv">
+                            <a href="" className="logo flex">
+                                <MdOutlineTravelExplore className="icon" />{" "}
+                                Travel.
+                            </a>
+                        </div>
+                        <div className="footerParagraph">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Atque aliquam quas perspiciatis quae quisquam
+                            dolor.
+                        </div>
+                        <div className="footerSocials flex">
+                            <AiOutlineTwitter className="icon" />
+                            <AiFillYoutube className="icon" />
+                            <AiFillInstagram className="icon" />
+                            <FaTripadvisor className="icon" />
+                        </div>
+                    </div>
+                    <div className="footerLinks grid">
+                      {/* group one */}
+                        <div className="linkGroup">
+                            <span className="groupTitle">OUR AGENCY</span>
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Services
+                            </li>
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Insurance
+                            </li>
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Agency
+                            </li>
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Tourism
+                            </li>
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Payment
+                            </li>
+                        </div>
+                      {/* group two */}
+                        <div className="linkGroup">
+                            <span className="groupTitle">PARTNERS</span>
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Bookings
+                            </li>
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Rentcars
+                            </li>
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                HostelWorld
+                            </li>
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Trivago
+                            </li>
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                TripAdvisor
+                            </li>
+                        </div>
+                      {/* group three */}
+                        <div className="linkGroup">
+                            <span className="groupTitle">LAST MINUTE</span>
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                London
+                            </li>
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                California
+                            </li>
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Indonesia
+                            </li>
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Europe
+                            </li>
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Istanbul
+                            </li>
+                        </div>
+                        <div className="footerDiv flex">
+                          <small>BEST TRAVEL WEBSITE THEME</small>
+                          <small>COPYRİGHTS RESERVED - ALBAYRAK 2024</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
 
-export default Footer
+export default Footer;
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,10 +1,111 @@
-import React from 'react'
-import "./main.css"
+import React from "react";
+import "./main.css";
+import photo from "../Assets/photo.jpg";
+import { CiLocationOn } from "react-icons/ci";
+import { FaClipboardList } from "react-icons/fa6";
 
+const Data = [
+    {
+        id: 1,
+        imgSrc: photo,
+        desTitle: "Turkey",
+        location: "Istanbul",
+        grade: "Culturel Relax",
+        fees: "$300",
+        description:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur officiis inventore sapiente modi quis totam?",
+    },
+    {
+        id: 2,
+        imgSrc: photo,
+        desTitle: "France",
+        location: "Paris",
+        grade: "Historical Adventure",
+        fees: "$400",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur officiis inventore sapiente modi quis totam?",
+    },
+    {
+        id: 3,
+        imgSrc: photo,
+        desTitle: "Japan",
+        location: "Tokyo",
+        grade: "Modern Exploration",
+        fees: "$500",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur officiis inventore sapiente modi quis totam?",
+    },
+    {
+        id: 4,
+        imgSrc: photo,
+        desTitle: "Italy",
+        location: "Rome",
+        grade: "Artistic Journey",
+        fees: "$350",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur officiis inventore sapiente modi quis totam?",
+    },
+    {
+        id: 5,
+        imgSrc: photo,
+        desTitle: "Australia",
+        location: "Sydney",
+        grade: "Nature Retreat",
+        fees: "$450",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur officiis inventore sapiente modi quis totam?",
+    }
+];
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
-}
+    return (
+        <section className="main container section">
+            <div className="secTitle">
+                <h3 className="title">Most visited destinations</h3>
+            </div>
+            <div className="secContent grid">
+                {Data.map(
+                    ({
+                        id,
+                        imgSrc,
+                        desTitle,
+                        location,
+                        grade,
+                        fees,
+                        description,
+                    }) => {
+                        return (
+                            <div key={id} className="singleDestination">
+                                <div className="imageDiv">
+                                    <img src={photo} alt={desTitle} />
+                                </div>
+                                <div className="cardInfo">
+                                    <h4 className="desTitle">{desTitle}</h4>
+                                    <span className="continent flex">
+                                        <CiLocationOn className="icon" />
+                                        <span className="name">{location}</span>
+                                    </span>
+                                    <div className="fees flex">
+                                        <div className="grade">
+                                            <span>
+                                                {grade}
+                                                <small>+1</small>
+                                            </span>
+                                        </div>
+                                        <div className="price">
+                                            <h5>{fees}</h5>
+                                        </div>
+                                    </div>
+                                    <div className="desc">
+                                        <p>{description}</p>
+                                    </div>
+                                    <button className="btn flex">
+                                        DETAILS
+                                        <FaClipboardList className="icon" />
+                                    </button>
+                                </div>
+                            </div>
+                        );
+                    }
+                )}
+            </div>
+        </section>
+    );
+};
 
-export default Main
+export default Main;
